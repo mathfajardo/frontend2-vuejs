07e7fef8cb2a3ff450ae1f99c48db550895e92c9
Matheus Fajardo <mathfajardo.contato@hotmail.com>
2025-12-16 14:44:37 -0300

    Update http.js

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://api.fajatech.com.br/api',
+    baseURL: 'https://api.fajatech.com.br/api',
     headers: {
         'Content-Type': 'application/json'
     }
