2a4f602efd2cf272c0da038f6fc6846e7d2be639
mathfajardo <mathfajardo.contato@hotmail.com>
2025-12-15 21:33:27 -0300

    ajuste http

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://69.6.221.138:8080/api',
+    baseURL: 'http://69.6.221.138:8000/api',
     headers: {
         'Content-Type': 'application/json'
     }
