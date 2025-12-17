a6bc5bd46a039f3d721c68ce5324a173b8378e87
mathfajardo <mathfajardo.contato@hotmail.com>
2025-12-17 18:55:31 -0300

    fix-mudando-img-notfound

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -6,7 +6,7 @@
 <template>
     <div class="d-flex flex-column align-items-center justify-content-center vh-100">
         <h1 >Rota não encontrada 404</h1>
-        <img src="../assets/img/ney_chorando.jpeg" alt="" class="img-thumbnail" width="60%">
+        <img src="../assets/img/ghost.jpg" alt="" class="img-thumbnail" width="40%">
         <RouterLink to="/" class="btn btn-primary mt-3">Ir para o login</RouterLink>
     </div>
     
