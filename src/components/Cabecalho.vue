f5acee63516df02919060e35567e9f703813d992
mathfajardo <mathfajardo.contato@hotmail.com>
2025-12-25 20:45:30 -0300

    feat- adicionando mais links no cabeçalho

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -33,26 +33,40 @@ async function fazerLogout() {
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <RouterLink class="nav-link" active-class="text-success" to="/produtos">Produtos</RouterLink>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Produtos
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" >
+            <li><RouterLink to="/produtos" class="dropdown-item" active-class="text-success">Visualizar produtos</RouterLink></li> 
+            <li><RouterLink to="/cadastroprodutos" class="dropdown-item" active-class="text-success">Cadastrar Produtos</RouterLink></li>
+          </ul>
         </li>
 
-        <li class="nav-item">
-          <RouterLink class="nav-link" active-class="text-success" to="/clientes">Clientes</RouterLink>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Clientes
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" >
+            <li><RouterLink to="/clientes" class="dropdown-item" active-class="text-success">Visualizar Clientes</RouterLink></li>
+            <li><RouterLink to="/cadastroclientes" class="dropdown-item" active-class="text-success">Cadastrar Clientes</RouterLink></li>
+          </ul>
         </li>
 
-        <li class="nav-item">
-          <RouterLink class="nav-link" active-class="text-success" to="/vendas">Vendas</RouterLink>
-        </li>
-
-        <li class="nav-item">
-          <RouterLink class="nav-link" active-class="text-success" to="/pdv">PDV</RouterLink>
+        <li class="nav-item dropdown me-5">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PDV
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" >
+            <li><RouterLink to="/vendas" class="dropdown-item" active-class="text-success">Visualizar vendas</RouterLink></li>
+            <li><RouterLink to="/pdv" class="dropdown-item" active-class="text-success">Registrar venda</RouterLink></li>
+          </ul>
         </li>
 
         
 
         <li class="nav-item">
-          <a class="nav-link text-danger" @click.prevent="fazerLogout" style="cursor: pointer;">Logout</a>
+          <a class="nav-link text-danger" @click.prevent="fazerLogout" style="cursor: pointer;"><i class="bi bi-box-arrow-right"></i></a>
         </li>
       </ul>
     </div>
@@ -61,3 +75,9 @@ async function fazerLogout() {
 
 
 </template>
+
+<style scoped>
+.dropdown-item:focus:active {
+  background-color: rgba(255, 255, 255, 0.15) !important;
+}
+</style>
